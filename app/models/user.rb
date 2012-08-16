48941271397ef509bde836f069defe0a08ba9180
Regis A. Fleck <regisfleck@gmail.com>
2012-08-16 13:04:17 -0300

    changed default locale to pt-BR

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,11 @@ class User < ActiveRecord::Base
   validates :login, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  private
+     def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_Base64
+    end
 end
